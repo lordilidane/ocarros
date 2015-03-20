6f708bcca0a1f1ea5c13459829e797c4a0574d34
jefferson13 <jefferson_roa@hotmail.es>
2015-03-20 09:55:39 -0500

    se corregio base de datos

--- base-de-datos/base_ocarros.sql
+++ base-de-datos/base_ocarros.sql
@@ -7,7 +7,7 @@
 #  Administrador de Base de Datos: MySQL/MariaDB
 #  Diagrama: ocarros
 #  Autor: Jefferson
-#  Fecha y hora: 19/03/2015 14:06:17
+#  Fecha y hora: 20/03/2015 9:52:31
 
 # GENERANDO TABLAS
 CREATE TABLE `historia_clinica` (
@@ -19,8 +19,6 @@ CREATE TABLE `historia_clinica` (
 	`nombre_comun` VARCHAR(50) NOT NULL,
 	`sexo` VARCHAR(20) NOT NULL,
 	`identificacion` VARCHAR(25) NOT NULL,
-	`medicos_veterinarios_id/mp` INTEGER NOT NULL,
-	KEY(`medicos_veterinarios_id/mp`),
 	PRIMARY KEY(`identificacion`)
 ) ENGINE=INNODB;
 CREATE TABLE `anamnesicos` (
@@ -52,8 +50,8 @@ CREATE TABLE `examen_clinico` (
 	`actitud` VARCHAR(10) NOT NULL,
 	`peso` VARCHAR(10) NOT NULL,
 	`cc` VARCHAR(10) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `general` (
 	`estado_general` VARCHAR(30) NOT NULL,
@@ -69,8 +67,8 @@ CREATE TABLE `general` (
 	`linfoide` VARCHAR(30) NOT NULL,
 	`organos_sentidos` VARCHAR(30) NOT NULL,
 	`otros` VARCHAR(30) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `ENTIDAD_5` (
 	`esi` VARCHAR(4) NOT NULL,
@@ -80,19 +78,23 @@ CREATE TABLE `ENTIDAD_5` (
 	`pronostico` VARCHAR(50) NOT NULL,
 	`cuarentena` VARCHAR(1) NOT NULL,
 	`tiempo_cuarentena` VARCHAR(20) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `movimiento_animal` (
 	`fecha` DATE NOT NULL,
 	`exhibicion` VARCHAR(200) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
-CREATE TABLE `medicos_veterinarios` (
-	`nombre` VARCHAR(50) NOT NULL,
-	`id/mp` INTEGER NOT NULL,
-	PRIMARY KEY(`id/mp`)
+CREATE TABLE `registro` (
+	`id_registro` INTEGER NOT NULL,
+	`fecha` DATE NOT NULL,
+	`responsable` VARCHAR(50) NOT NULL,
+	`id_responsable/mp` INTEGER NOT NULL,
+	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
+	KEY(`historia_clinica_identificacion`),
+	PRIMARY KEY(`id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `diagnosticos` (
 	`hallazgos_anormales` VARCHAR(500) NOT NULL,
@@ -100,15 +102,15 @@ CREATE TABLE `diagnosticos` (
 	`planes_diagnosticos` VARCHAR(500) NOT NULL,
 	`planes_terapeuticos` VARCHAR(500) NOT NULL,
 	`otros_planes` VARCHAR(500) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `marcaje` (
 	`marcaje` VARCHAR(50) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`),
 	`tipo_marcaje_id_tipo_marcaje` INTEGER NOT NULL,
-	KEY(`tipo_marcaje_id_tipo_marcaje`)
+	KEY(`tipo_marcaje_id_tipo_marcaje`),
+	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
+	KEY(`historia_clinica_identificacion`)
 ) ENGINE=INNODB;
 CREATE TABLE `tipo_marcaje` (
 	`id_tipo_marcaje` INTEGER NOT NULL,
@@ -118,22 +120,15 @@ CREATE TABLE `tipo_marcaje` (
 CREATE TABLE `terapeutica_evolucion` (
 	`fecha` DATE NOT NULL,
 	`evolucion` VARCHAR(2000) NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
 ) ENGINE=INNODB;
 CREATE TABLE `examenes_solicitados` (
-	`biometria_hematica` VARCHAR(10) NOT NULL,
-	`perfil_hepatico` VARCHAR(10) NOT NULL,
-	`perfil_renal` VARCHAR(10) NOT NULL,
-	`hormonas` VARCHAR(10) NOT NULL,
-	`coprologico` VARCHAR(10) NOT NULL,
-	`coprocultivo` VARCHAR(10) NOT NULL,
-	`hemoparasitos` VARCHAR(10) NOT NULL,
-	`rx` VARCHAR(10) NOT NULL,
-	`ecografia` VARCHAR(10) NOT NULL,
-	`fecha` DATE NOT NULL,
-	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
-	KEY(`historia_clinica_identificacion`)
+	`id_examen` INTEGER NOT NULL,
+	`nombre_examen` VARCHAR(50) NOT NULL,
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`),
+	PRIMARY KEY(`id_examen`)
 ) ENGINE=INNODB;
 CREATE TABLE `valoracion_nutricional` (
 	`fecha` DATE NOT NULL,
@@ -166,20 +161,27 @@ CREATE TABLE `valoracion_nutricional` (
 	`dieta_ingredientes` VARCHAR(200) NOT NULL,
 	`dieta_aceptacion_alimentos` VARCHAR(200) NOT NULL,
 	`dieta_suplementacion` VARCHAR(200) NOT NULL,
+	`registro_id_registro` INTEGER NOT NULL,
+	KEY(`registro_id_registro`)
+) ENGINE=INNODB;
+CREATE TABLE `origen` (
+	`id_origen` INTEGER NOT NULL,
+	`tipo_origen` VARCHAR(50) NOT NULL,
 	`historia_clinica_identificacion` VARCHAR(25) NOT NULL,
 	KEY(`historia_clinica_identificacion`)
 ) ENGINE=INNODB;
 
 # GENERANDO RELACIONES
-ALTER TABLE `historia_clinica` ADD CONSTRAINT `historia_clinica_medicos_veterinarios_medicos_veterinarios_id/1` FOREIGN KEY (`medicos_veterinarios_id/mp`) REFERENCES `medicos_veterinarios`(`id/mp`) ON DELETE NO ACTION ON UPDATE CASCADE;
 ALTER TABLE `anamnesicos` ADD CONSTRAINT `anamnesicos_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `examen_clinico` ADD CONSTRAINT `examen_clinico_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `general` ADD CONSTRAINT `general_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `ENTIDAD_5` ADD CONSTRAINT `entidad_5_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `movimiento_animal` ADD CONSTRAINT `movimiento_animal_historia_clinica_historia_clinica_identifica6` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `diagnosticos` ADD CONSTRAINT `diagnosticos_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `marcaje` ADD CONSTRAINT `marcaje_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `examen_clinico` ADD CONSTRAINT `examen_clinico_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `general` ADD CONSTRAINT `general_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `ENTIDAD_5` ADD CONSTRAINT `entidad_5_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `movimiento_animal` ADD CONSTRAINT `movimiento_animal_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `registro` ADD CONSTRAINT `registro_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `diagnosticos` ADD CONSTRAINT `diagnosticos_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
 ALTER TABLE `marcaje` ADD CONSTRAINT `marcaje_tipo_marcaje_tipo_marcaje_id_tipo_marcaje` FOREIGN KEY (`tipo_marcaje_id_tipo_marcaje`) REFERENCES `tipo_marcaje`(`id_tipo_marcaje`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `terapeutica_evolucion` ADD CONSTRAINT `terapeutica_evolucion_historia_clinica_historia_clinica_ident10` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `examenes_solicitados` ADD CONSTRAINT `examenes_solicitados_historia_clinica_historia_clinica_identi11` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
-ALTER TABLE `valoracion_nutricional` ADD CONSTRAINT `valoracion_nutricional_historia_clinica_historia_clinica_iden12` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `marcaje` ADD CONSTRAINT `marcaje_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `terapeutica_evolucion` ADD CONSTRAINT `terapeutica_evolucion_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `examenes_solicitados` ADD CONSTRAINT `examenes_solicitados_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `valoracion_nutricional` ADD CONSTRAINT `valoracion_nutricional_registro_registro_id_registro` FOREIGN KEY (`registro_id_registro`) REFERENCES `registro`(`id_registro`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `origen` ADD CONSTRAINT `origen_historia_clinica_historia_clinica_identificacion` FOREIGN KEY (`historia_clinica_identificacion`) REFERENCES `historia_clinica`(`identificacion`) ON DELETE NO ACTION ON UPDATE CASCADE;
